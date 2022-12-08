ac3e7cf53b6bb65a68f669a71ddd6512ba1b66ef
Melisa Koc <melisakoc.de@gmail.com>
2022-12-08 23:28:48 +0100

    kurulum

---
+++ src/test/java/IlkClass.java
@@ -0,0 +1,15 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class IlkClass {
+
+    public static void main(String[] args) {
+
+        System.setProperty("webdriver.chrome.driver","src/resources/drivers/chromedriver.exe");
+
+        WebDriver driver = new ChromeDriver();
+
+        driver.get("http://www.mervekokturk.com");
+
+    }
+}
